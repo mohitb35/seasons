51ecdda369b674706dd6ff38ae83af4437482c4c
Mohit Bajaj <mohit_b35@hotmail.com>
2020-04-10 14:42:29 +0530

    Initial scaffolding + fetch latitude --> navigator geolocation api used

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import SeasonDisplay from './SeasonDisplay';
+
+const App = () => {
+	let latitude;
+	window.navigator.geolocation.getCurrentPosition(
+		(position) => {
+			latitude = position.coords.latitude;
+			console.log(position.coords.latitude);
+		},
+		(err) => {
+			console.log(err);	
+		}
+	)
+
+	return (
+		<div>Hi there!
+			<SeasonDisplay />
+			{latitude}
+		</div>
+	)
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<App />,
+	document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
