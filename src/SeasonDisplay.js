21d93c1970b46042f08e0d4a1a088db7b97132cb
Mohit Bajaj <mohit_b35@hotmail.com>
2020-04-13 13:06:04 +0530

    refactor - replace ternary exp. with config object

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+	summer: {
+		text: 'Let\'s hit the beach',
+		iconName: 'sun' 
+	},
+	winter: {
+		text: 'Brrr...it\'s chilly',
+		iconName: 'snowflake' 
+	}
+}
+
 const getSeason = (lat, month) => {
 	if (month > 2 && month < 9) {
 		return lat > 0 ? 'summer' : 'winter';
@@ -11,15 +22,14 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
 	const season = getSeason(props.lat, new Date().getMonth());
-	
-	const text = season === 'winter' ? 'Brrr...it\'s chilly' : 'Let\'s hit the beach';
-	const icon = season === 'winter' ? 'snowflake icon' : 'sun icon'; 
+
+	const { text, iconName } = seasonConfig[season];
 	
 	return (
 		<div>
-			<i className = {icon}></i>
+			<i className = {`${iconName} icon`}></i>
 			<h1>{text}</h1>
-			<i className = {icon}></i>
+			<i className = {`${iconName} icon`}></i>
 		</div>
 	)
 }
